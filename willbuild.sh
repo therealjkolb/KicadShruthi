15d25e0158dc8e720378f77b06af953d7da2aa76
Wfkolb <wfkolb@gmail.com>
2023-10-08 11:48:31 -0400

    Added a script to combine build steps

---
+++ willbuild.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#CLEAN EVERYTHING
+rm -rf ./build/*
+
+#Build shurthi (Check the AVRLIB_TOOLS_PATH to make sure it matches your computer)
+make AVRLIB_TOOLS_PATH='/cygdrive/d/Development/avr8-gnu-toolchain-win32_x86_64/bin/'
+make -f ./bootloader/makefile AVRLIB_TOOLS_PATH='/cygdrive/d/Development/avr8-gnu-toolchain-win32_x86_64/bin/'
+
+
+
+
+
+##Utility commands that I wanted to keep around:
+
+#make clean AVRLIB_TOOLS_PATH='/cygdrive/d/Development/avr8-gnu-toolchain-win32_x86_64/bin/' -j1
+#make AVRLIB_TOOLS_PATH='/cygdrive/d/Development/avr8-gnu-toolchain-win32_x86_64/bin/' -j1 
+#rm -rf ./bootloader/build/*
+#/cygdrive/d/Development/avr8-gnu-toolchain-win32_x86_64/bin/avr-g++ -mmcu=atmega644p -I. -I./devices -g -Os -w -Wall -DF_CPU=20000000 -D__PROG_TYPES_COMPAT__ -fdata-sections -ffunction-sections -fshort-enums -fno-move-loop-invariants -DDISABLE_DEFAULT_UART_RX_ISR  -DATMEGA644P -DSERIAL_RX_0 -mcall-prologues -fno-exceptions -c -MT ./bootloader/bootloader.cc -MMD -MP -MF ./bootloader/build/muboot/bootloader.d -o ./bootloader/build/muboot/bootloader.o ./bootloader/bootloader.cc 
+
+#build the bootloader (the make system is really bugged here, so instead of rearchetecting I just hard coded the g++ cmd)
+#Note there are several paths here
+#Verify the path to your avr-g++ (i'm on 7.3.0)
+
+#/cygdrive/d/Development/avr8-gnu-toolchain-win32_x86_64/bin/avr-g++ -mmcu=atmega644p -I.  -I//cygdrive//d//Development//jakeBoard//KicadShruthi//devices// -I//cygdrive//d//Development//jakeBoard//KicadShruthi// -I/cygdrive/d/Development/jakeBoard/KicadShruthi/avrlib/devices -g -Os -w -Wall -DF_CPU=20000000 -D__PROG_TYPES_COMPAT__ -fdata-sections -ffunction-sections -fshort-enums -fno-move-loop-invariants -DDISABLE_DEFAULT_UART_RX_ISR  -DATMEGA644P -DSERIAL_RX_0 -mcall-prologues -fno-exceptions -c -MT ./bootloader/bootloader.cc -MMD -MP -MF ./bootloader/build/muboot/bootloader.d -o ./bootloader/build/muboot/bootloader.o ./bootloader/bootloader.cc 
+#/cygdrive/d/Development/avr8-gnu-toolchain-win32_x86_64/bin/avr-g++ -mmcu=atmega644p -I. -I./devices -g -Os -w -Wall -DF_CPU=20000000 -D__PROG_TYPES_COMPAT__ -fdata-sections -ffunction-sections -fshort-enums -fno-move-loop-invariants -DDISABLE_DEFAULT_UART_RX_ISR  -DATMEGA644P -DSERIAL_RX_0 -mcall-prologues -fno-exceptions -c -MT ./bootloader/bootloader.cc -MMD -MP -MF ./bootloader/build/muboot/bootloader.d -o ./bootloader/build/muboot/bootloader.o ./bootloader/bootloader.cc 
+
+
+#Copy the build from the bootloader folder to the ./build folder
+#cp -r ./bootloader/build/muboot ./build/
+
+
+
+
